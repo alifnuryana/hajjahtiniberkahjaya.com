0396bc182d78747b701ce66baccc6a2c0c5c203b
Alif Nuryana <alifnuryana0@gmail.com>
2024-12-10 15:56:22 +0700

    feat: setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-11-01',
+
+  eslint: {
+    checker: true,
+    config: {
+      stylistic: true,
+    },
+  },
 })
