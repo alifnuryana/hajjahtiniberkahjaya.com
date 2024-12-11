dbbba39c5ff3a9fa6db34895db6417ee84a4cf48
Alif Nuryana <alifnuryana0@gmail.com>
2024-12-11 22:18:11 +0700

    feat: update SEO meta configuration and restructure app layout

--- app/app.vue
+++ app/app.vue
@@ -2,19 +2,6 @@
 const appConfig = useAppConfig()
 const runtimeConfig = useRuntimeConfig()
 
-useSeoMeta({
-  titleTemplate: title => title ? `${title} | ${appConfig.title}` : appConfig.title,
-  description: appConfig.description,
-  ogTitle: appConfig.title,
-  ogDescription: appConfig.description,
-  ogImage: '/images/og-image.png',
-  ogUrl: runtimeConfig.public.appUrl,
-  twitterTitle: appConfig.title,
-  twitterDescription: appConfig.description,
-  twitterImage: '/images/og-image.png',
-  twitterCard: 'summary_large_image',
-})
-
 useHead({
   htmlAttrs: {
     lang: 'id',
@@ -27,11 +14,30 @@ useHead({
     },
   ],
 })
+
+useSeoMeta({
+  titleTemplate: '%s %separator PT. Hajjah Tini Berkah Jaya',
+  description: appConfig.description,
+
+  ogTitle: '%s',
+  ogDescription: appConfig.description,
+  ogImage: '/images/og-image.png',
+  ogUrl: runtimeConfig.public.appUrl,
+  ogType: 'website',
+
+  twitterTitle: '%s',
+  twitterDescription: appConfig.description,
+  twitterImage: '/images/og-image.png',
+  twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
   <NuxtLoadingIndicator />
-  <NuxtLayout>
+  <div class="content-wrapper">
+    <header class="wrapper bg-dark">
+      <AppNavbar />
+    </header>
     <NuxtPage />
-  </NuxtLayout>
+  </div>
 </template>
