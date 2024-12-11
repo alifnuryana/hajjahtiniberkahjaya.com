f74aa719ffd8d2817ffeb36a7fdabb932e568892
Alif Nuryana <alifnuryana0@gmail.com>
2024-12-11 22:56:45 +0700

    feat: update logo image sizes for responsive design

--- app/components/App/Navbar/Brand.vue
+++ app/components/App/Navbar/Brand.vue
@@ -4,13 +4,13 @@
       <NuxtImg
         class="logo-dark"
         src="/images/logo-light.png"
-        sizes="60px"
+        sizes="60px md:70px"
         alt="Logo PT Hajjah Tini Berkah Jaya"
       />
       <NuxtImg
         class="logo-light"
         src="/images/logo-dark.png"
-        sizes="60px"
+        sizes="60px md:70px"
         alt="Logo PT Hajjah Tini Berkah Jaya"
       />
     </NuxtLink>
