7d0ba73e1fc9ef21ea3561a60444c7f84d56e9e7
Alif Nuryana <alifnuryana0@gmail.com>
2024-12-12 02:47:23 +0700

    feat: integrate GLightbox and Plyr for enhanced video handling in VideoBox component

--- app/components/Section/Hero/VideoBox.vue
+++ app/components/Section/Hero/VideoBox.vue
@@ -1,3 +1,45 @@
+<script setup lang="ts">
+onMounted(() => {
+  window.GLightbox({
+    selector: '*[data-glightbox]',
+    touchNavigation: true,
+    loop: false,
+    zoomable: false,
+    autoplayVideos: true,
+    moreLength: 0,
+    plyr: {
+      css: '',
+      js: '',
+      config: {
+        ratio: '',
+        fullscreen: {
+          enabled: false,
+          iosNative: false,
+        },
+        youtube: {
+          noCookie: true,
+          rel: 0,
+          showinfo: 0,
+          iv_load_policy: 3,
+        },
+        vimeo: {
+          byline: false,
+          portrait: false,
+          title: false,
+          transparent: false,
+        },
+      },
+    },
+  })
+
+  window.Plyr.setup('.player', {
+    loadSprite: true,
+    fullscreen: { enabled: true, iosNative: true },
+    muted: false,
+  })
+})
+</script>
+
 <template>
   <div
     class="col-lg-5 offset-lg-1 mb-n18"
@@ -9,7 +51,9 @@
         class="btn btn-circle btn-primary btn-play ripple mx-auto mb-6 position-absolute"
         style="top:50%; left: 50%; transform: translate(-50%,-50%); z-index:3;"
         data-glightbox
-      ><i class="icn-caret-right" /></a>
+      >
+        <i class="icn-caret-right" />
+      </a>
       <figure class="rounded shadow-lg">
         <NuxtImg
           src="/images/about13.jpg"
