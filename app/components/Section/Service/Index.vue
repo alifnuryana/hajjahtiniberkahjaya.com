8e8db6e43e367f9366d831712bafa8b0bf0a6074
Alif Nuryana <alifnuryana0@gmail.com>
2024-12-12 10:44:55 +0700

    feat: new services section

---
+++ app/components/Section/Service/Index.vue
@@ -0,0 +1,9 @@
+<template>
+  <SectionServiceHeader />
+  <div class="row gx-md-8 gy-8 mb-14 mb-md-18">
+    <SectionServiceItem />
+    <SectionServiceItem />
+    <SectionServiceItem />
+    <SectionServiceItem />
+  </div>
+</template>
